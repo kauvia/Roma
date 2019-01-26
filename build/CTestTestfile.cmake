7f5441e302c73d34b08ad41b3ba47d1fee06b165
kauvia <mkhw1989@gmail.com>
2019-01-26 22:30:05 +0800

    untracked build folder

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kau/prog/cpp/Roma
-# Build directory: /home/kau/prog/cpp/Roma/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Engine")
